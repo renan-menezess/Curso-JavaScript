40433e0d7b35edc68e3fd9861cb074adbb9b1029
Renan Menezes <2003rcsm@gmail.com>
2023-08-31 01:08:52 -0300

    Laço limitado por um contador

---
+++ script/laco-limitador.js
@@ -0,0 +1,11 @@
+/* Objetivo: Escrever uma mensagem limitando-a em 10 vezes através de um contador que será
+incrementado de mais 1
+*/
+/* Definicao das variaveis */
+var js_contador=1;
+/* Laço de Repetição Condicional - while */
+while (js_contador <= 10){
+    document.write('<p>' + js_contador + '&ordf; Repetição de uma única linha de comando dentro de um laço condicional: ');
+    js_contador +=1;
+}
+   
